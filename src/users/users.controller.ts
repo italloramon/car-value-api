2bb0e9d00a8a3d0b86845ffabb14c538f5a0cde6
italloramon <irvp@ic.ufal.br>
2023-06-04 09:53:00 -0300

    refactor: controller-wide serialization and a bit of type safety around Serialize

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -14,6 +14,7 @@ import { UpdateUserDto } from './dtos/update-user.dto';
 import { Serialize } from '../interceptors/serialize.interceptor';
 import { UserDto } from './dtos/user.dto';
 
+@Serialize(UserDto)
 @Controller('auth')
 export class UsersController {
   constructor(private userService: UsersService) {}
@@ -23,7 +24,6 @@ export class UsersController {
     return this.userService.create(body.email, body.password);
   }
 
-  @Serialize(UserDto)
   @Get(':id')
   findUser(@Param('id') id: string) {
     console.log('handler is running');
