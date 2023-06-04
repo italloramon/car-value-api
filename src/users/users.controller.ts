950482b555a695e0b9826305e44878a5d039b2ca
italloramon <irvp@ic.ufal.br>
2023-06-04 15:48:16 -0300

    feat: implementing signup functionality

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -13,15 +13,19 @@ import { UsersService } from './users.service';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { Serialize } from '../interceptors/serialize.interceptor';
 import { UserDto } from './dtos/user.dto';
+import { AuthService } from './auth.service';
 
 @Serialize(UserDto)
 @Controller('auth')
 export class UsersController {
-  constructor(private userService: UsersService) {}
+  constructor(
+    private userService: UsersService,
+    private authService: AuthService,
+  ) {}
 
   @Post('signup')
   createUser(@Body() body: CreateUserDto) {
-    return this.userService.create(body.email, body.password);
+    return this.authService.signup(body.email, body.password);
   }
 
   @Get(':id')
