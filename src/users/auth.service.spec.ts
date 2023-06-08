efa15908d976454041ec85678e33c5a5c6919a2e
italloramon <irvp@ic.ufal.br>
2023-06-08 20:31:20 -0300

    test: adding throws an error if email already in use

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -2,12 +2,14 @@ import { Test } from '@nestjs/testing';
 import { User } from './user.entity';
 import { UsersService } from './users.service';
 import { AuthService } from './auth.service';
+import { BadRequestException } from '@nestjs/common';
 
 describe('AuthService', () => {
   let service: AuthService;
+  let fakeUsersService: Partial<UsersService>;
 
   beforeEach(async () => {
-    const fakeUsersService: Partial<UsersService> = {
+    fakeUsersService = {
       find: () => Promise.resolve([]),
       create: (email: string, password: string) =>
         Promise.resolve({ id: 1, email, password } as User),
@@ -37,5 +39,13 @@ describe('AuthService', () => {
     const [salt, hash] = user.password.split('.');
     expect(salt).toBeDefined();
     expect(hash).toBeDefined();
+  });
+
+  it('throws an error if user signs up with email that is in use', async () => {
+    fakeUsersService.find = () =>
+      Promise.resolve([{ id: 1, email: 'a', password: 'a' } as User]);
+    await expect(service.signup('test@test.com', 'test123')).rejects.toThrow(
+      BadRequestException,
+    );
   });
 });
